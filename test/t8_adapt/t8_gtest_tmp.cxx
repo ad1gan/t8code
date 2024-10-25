cceab7f76cf3d419e420f6329e5db19da7f27f4e
Adrian Kolb <kolb@eddy.rwth-aachen.de>
2024-10-25 09:47:40 +0200

    dummy test for mra

---
+++ test/t8_adapt/t8_gtest_tmp.cxx
@@ -0,0 +1,22 @@
+
+#include <gtest/gtest.h>
+
+#include <t8_adapt/multiscale.hpp>
+
+class mra_test : public testing::Test {
+  void SetUp() {}
+
+ public:
+  const int max_level = 5;
+  const int p = 3;
+};
+
+TEST_F(mra_test_fixture, cstr) {
+  t8_mra::t8_multiscale mra(max_level, p);
+
+  ASSERT_EQ(max_level, mra.max_level)
+  ASSERT_EQ(p, mra.polynomial_degree);
+  ASSERT_EQ((p * (p + 1)) / 2, mra.dof);
+  ASSERT_EQ(mra.mask_coeffs.size(), 4);
+  ASSERT_EQ(mra.inv_mask_coeffs.size(), 4);
+}
