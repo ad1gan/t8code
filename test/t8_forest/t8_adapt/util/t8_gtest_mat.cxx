38111583cf63c6dfc259efbb8772d1ae6220fdd1
Adrian Kolb <kolb@eddy.rwth-aachen.de>
2024-10-25 11:12:25 +0200

    add files for vec and mat tests

---
+++ test/t8_forest/t8_adapt/util/t8_gtest_mat.cxx
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include <t8_forest/t8_adapt/util/mat.hpp>
+
+class t8_adapt_mat_test : public ::testing::Test {
+  void SetUp() {}
+
+ public:
+};
+
+TEST_F(t8_adapt_mat_test, cstr) {}
---
+++ test/t8_forest/t8_adapt/util/t8_gtest_vec.cxx
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include <t8_forest/t8_adapt/util/vec.hpp>
+
+class t8_adapt_vec_test : public ::testing::Test {
+  void SetUp() {}
+
+ public:
+};
+
+TEST_F(t8_adapt_vec_test, cstr) {}
