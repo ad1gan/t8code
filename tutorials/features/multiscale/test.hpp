edd8fd31f2a4c0bcb452d22073fcbbfad1e5c020
Adrian Kolb <kolb@eddy.rwth-aachen.de>
2024-12-20 10:52:45 +0100

    working on example

--- tutorials/features/multiscale/test.hpp
+++ tutorials/features/multiscale/test.hpp
@@ -27,12 +27,105 @@
 #include <t8.h>
 #include <t8_forest/t8_forest_general.h>
 
+#include <iostream>
 #include <t8_cmesh.hxx>
 #include <t8_forest/t8_adapt/multiscale.hpp>
+#include <t8_geometry/t8_geometry_implementations/t8_geometry_linear.hxx>
+#include <t8_schemes/t8_default/t8_default.hxx>
+
+void output_test(const t8_mra::t8_multiscale<T8_ECLASS_TRIANGLE>& mst) {
+  std::cout << "poly_degree: " << mst.polynomial_degree << "\n";
+  std::cout << "dof: " << mst.dof << "\n";
+  std::cout << "max_level: " << mst.max_level << "\n";
+  std::cout << "c_thres: " << mst.c_thresh << "\n";
+
+  for (auto i = 0u; i < mst.mask_coeffs.size(); ++i) {
+    for (auto j = 0u; j < mst.mask_coeffs[i].cols(); ++j) {
+      for (auto k = 0u; k < mst.mask_coeffs[i].rows(); ++k)
+        std::cout << mst.mask_coeffs[i](k, j) << " ";
+      std::cout << "\n";
+    }
+    std::cout << "\n\n";
+  }
+
+  for (auto i = 0u; i < mst.inv_mask_coeffs.size(); ++i) {
+    for (auto j = 0u; j < mst.inv_mask_coeffs[i].cols(); ++j) {
+      for (auto k = 0u; k < mst.inv_mask_coeffs[i].rows(); ++k)
+        std::cout << mst.inv_mask_coeffs[i](k, j) << " ";
+      std::cout << "\n";
+    }
+    std::cout << "\n\n";
+  }
+}
+
+/* This cmesh is [0,1]² with 8 triangles. */
+t8_cmesh_t t8_cmesh_new_basic(sc_MPI_Comm comm) {
+  double vertices[72] = {
+      0,   0,   0, 0.5, 0,   0, 0.5, 0.5, 0,  // triangle 2
+      0,   0,   0, 0,   0.5, 0, 0.5, 0.5, 0,  // triangle 1
+      0.5, 0,   0, 1,   0,   0, 1,   0.5, 0,  // triangle 3
+      0.5, 0,   0, 0.5, 0.5, 0, 1,   0.5, 0,  // triangle 4
+      0,   0.5, 0, 0.5, 0.5, 0, 0.5, 1,   0,  // triangle 5
+      0,   0.5, 0, 0,   1,   0, 0.5, 1,   0,  // triangle 6
+      0.5, 0.5, 0, 1,   0.5, 0, 1,   1,   0,  // triangle 7
+      0.5, 0.5, 0, 0.5, 1,   0, 1,   1,   0,  // triangle 8
+  };
+
+  t8_cmesh_t cmesh;
+  t8_cmesh_init(&cmesh);
+
+  t8_cmesh_register_geometry<t8_geometry_linear>(cmesh, 2);
+
+  /* 4. Definition of the classes of the different trees */
+  t8_cmesh_set_tree_class(cmesh, 0, T8_ECLASS_TRIANGLE);
+  t8_cmesh_set_tree_class(cmesh, 1, T8_ECLASS_TRIANGLE);
+  t8_cmesh_set_tree_class(cmesh, 2, T8_ECLASS_TRIANGLE);
+  t8_cmesh_set_tree_class(cmesh, 3, T8_ECLASS_TRIANGLE);
+  t8_cmesh_set_tree_class(cmesh, 4, T8_ECLASS_TRIANGLE);
+  t8_cmesh_set_tree_class(cmesh, 5, T8_ECLASS_TRIANGLE);
+  t8_cmesh_set_tree_class(cmesh, 6, T8_ECLASS_TRIANGLE);
+  t8_cmesh_set_tree_class(cmesh, 7, T8_ECLASS_TRIANGLE);
+
+  t8_cmesh_set_tree_vertices(cmesh, 0, vertices, 3);
+  t8_cmesh_set_tree_vertices(cmesh, 1, vertices + 9, 3);
+  t8_cmesh_set_tree_vertices(cmesh, 2, vertices + 18, 3);
+  t8_cmesh_set_tree_vertices(cmesh, 3, vertices + 27, 3);
+  t8_cmesh_set_tree_vertices(cmesh, 4, vertices + 36, 3);
+  t8_cmesh_set_tree_vertices(cmesh, 5, vertices + 45, 3);
+  t8_cmesh_set_tree_vertices(cmesh, 6, vertices + 54, 3);
+  t8_cmesh_set_tree_vertices(cmesh, 7, vertices + 63, 3);
+
+  t8_cmesh_set_join(cmesh, 0, 1, 1, 1, 0);
+  t8_cmesh_set_join(cmesh, 0, 3, 0, 2, 0);
+  t8_cmesh_set_join(cmesh, 1, 4, 0, 2, 0);
+  t8_cmesh_set_join(cmesh, 2, 3, 1, 1, 0);
+  t8_cmesh_set_join(cmesh, 3, 6, 0, 2, 0);
+  t8_cmesh_set_join(cmesh, 4, 5, 1, 1, 0);
+  t8_cmesh_set_join(cmesh, 4, 7, 0, 2, 0);
+  t8_cmesh_set_join(cmesh, 6, 7, 1, 1, 0);
+
+  t8_cmesh_commit(cmesh, comm);
+
+  return cmesh;
+}
 
 /// Only for testing
 int t8_tutorial_multiscale_main(int argc, char** argv) {
-  int bla = 0;
+  SC_CHECK_MPI(sc_MPI_Init(&argc, &argv));
 
-  return bla;
+  sc_init(sc_MPI_COMM_WORLD, 1, 1, NULL, SC_LP_PRODUCTION);
+  t8_init(SC_LP_PRODUCTION);
+  // sc_MPI_Comm comm = sc_MPI_COMM_WORLD;
+
+  t8_mra::t8_multiscale<T8_ECLASS_TRIANGLE> multiscale(3, 0.1, 6u);
+  output_test(multiscale);
+
+  // t8_cmesh_t cmesh = example_mesh(comm);
+  // t8_scheme_cxx_t* scheme = t8_scheme_new_default_cxx();
+
+  sc_finalize();
+
+  SC_CHECK_MPI(sc_MPI_Finalize());
+
+  return 0;
 }
